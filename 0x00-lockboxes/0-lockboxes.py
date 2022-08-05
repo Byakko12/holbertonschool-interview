8b08968cb77205bf71e751198648a6a06fc86984
Byakko12 <davidlandg@gmail.com>
2022-08-04 23:27:06 -0500

    [ADD] - lock boxes

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+
+"""
+Problem: You have n number of locked boxes in front of you.
+         Each box is numbered sequentially from 0 to n - 1
+         and each box may contain keys to the other boxes.
+Task: Write a method that determines if all the boxes can be opened.
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    Function that checks with boolean value if the list type and
+    length to invoke two for iterations one to traverse the list
+    and the other to compaer if key is idx or not in order to open
+    """
+
+    unlocked = [0]
+    for box_id, box in enumerate(boxes):
+        if not box:
+            continue
+        for key in box:
+            if key < len(boxes) and key not in unlocked and key != box_id:
+                unlocked.append(key)
+    if len(unlocked) == len(boxes):
+        return True
+    return False
+
+    return len(total) == len(boxes)
