d3382a95218991a51e1a1a5e9a537013a2c0fd04
Byakko12 <davidlandg@gmail.com>
2022-09-29 22:47:49 -0500

    [ADD ] files

---
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,24 @@
+#include "palindrome.h"
+/**
+ * is_palindrome - Function to check palindromes
+ * @n: Number to be checked
+ * Return: integer 1 or 0
+ */
+int is_palindrome(unsigned long n)
+{
+	unsigned long num, valr = 0;
+
+	num = n;
+	unsigned long palindrome = 1, no_palindrome = 0;
+
+	while (num != 0)
+	{
+		valr = valr * 10;
+		valr = valr + num % 10;
+		num = num / 10;
+	}
+	if (n == valr)
+		return (palindrome);
+	else
+		return (no_palindrome);
+}
